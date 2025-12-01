1864b6a0ec9c5e382de09ee4a26bf3284b67e452
Ccr1tt <3dfilamentfrenzy@gmail.com>
2025-12-01 15:19:42 -0800

    EssentialsX Support

--- src/main/java/org/crit/simpleUtils/SimpleUtils.java
+++ src/main/java/org/crit/simpleUtils/SimpleUtils.java
@@ -1,6 +1,7 @@
 package org.crit.simpleUtils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
@@ -9,6 +10,10 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.crit.simpleUtils.commands.*;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Objects;
+
 
 public final class SimpleUtils extends JavaPlugin implements Listener {
 
@@ -24,15 +29,54 @@ public final class SimpleUtils extends JavaPlugin implements Listener {
     public void onEnable() {
         saveDefaultConfig();
         reloadConfig();
+        Bukkit.getPluginManager().registerEvents(this,this);
+        registerCommands();
+        getLogger().info("Registered commands!");
+        System.out.println("Enabled!");
+        getLogger().warning("""
+                 $$$$$$\\  $$\\                         $$\\           $$\\   $$\\   $$\\     $$\\ $$\\\s
+                $$  __$$\\ \\__|                        $$ |          $$ |  $$ |  $$ |    \\__|$$ |
+                $$ /  \\__|$$\\ $$$$$$\\$$$$\\   $$$$$$\\  $$ | $$$$$$\\  $$ |  $$ |$$$$$$\\   $$\\ $$ |
+                \\$$$$$$\\  $$ |$$  _$$  _$$\\ $$  __$$\\ $$ |$$  __$$\\ $$ |  $$ |\\_$$  _|  $$ |$$ |
+                 \\____$$\\ $$ |$$ / $$ / $$ |$$ /  $$ |$$ |$$$$$$$$ |$$ |  $$ |  $$ |    $$ |$$ |
+                $$\\   $$ |$$ |$$ | $$ | $$ |$$ |  $$ |$$ |$$   ____|$$ |  $$ |  $$ |$$\\ $$ |$$ |
+                \\$$$$$$  |$$ |$$ | $$ | $$ |$$$$$$$  |$$ |\\$$$$$$$\\ \\$$$$$$  |  \\$$$$  |$$ |$$ |
+                 \\______/ \\__|\\__| \\__| \\__|$$  ____/ \\__| \\_______| \\______/    \\____/ \\__|\\__|
+                                            $$ |                                               \s
+                                            $$ |                                               \s
+                                            \\__|  \s
+                               My first minecraft plugin twin, Don't expect much!
+                """);
+    }
+    public void registerCommands() {
+
         getCommand("dupe").setExecutor(new DupeCommand(this));
         getCommand("fly").setExecutor(new FlyCommand(this));
         getCommand("dupeblacklist").setExecutor(new DupeBlacklist(this));
         getCommand("vanish").setExecutor(new VanishCommand(this));
         getCommand("enchant").setExecutor(new EnchantCommand(this));
-        Bukkit.getPluginManager().registerEvents(this,this);
+        Plugin[] plugins = Bukkit.getPluginManager().getPlugins();
+        Plugin essentials = getServer().getPluginManager().getPlugin("Essentials");
 
-        getLogger().info("Registered commands!");
-        System.out.println("Enabled!");
+        if (essentials != null) {
+            PluginCommand fly = getCommand("fly");
+
+            if (fly != null) {
+                getLogger().warning("""
+                        EssentialsX Detected disabled the following commands: 
+                            - /fly
+                        """);
+                fly.setExecutor((sender, cmd, label, args) -> {
+                    if (args.length > 0) {
+                        String otherPlayer = args[0];
+                        Bukkit.dispatchCommand(sender, "essentials:fly " + otherPlayer);
+                    } else {
+                        Bukkit.dispatchCommand(sender, "essentials:fly");
+                    }
+                    return true;
+                });
+            }
+        }
     }
 
     @Override
